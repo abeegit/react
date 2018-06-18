83a594bd5881744768932b4f80e9f1a648e33e2e
Abhijeet Kumar <abhijeet.kumar@22feettribalww.com>
2018-06-18 16:19:31 +0530

    Ejected app and added node-sass

---
+++ src/components/Playground/Playground.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Playground.scss';
+
+export default class Playground extends Component {
+    render() {
+        return (
+            <div className="playground" style={{ color: "red" }}>
+                Red
+            </div>
+        );
+    }
+}
